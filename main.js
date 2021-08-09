63bf2e35baff85a2c0a785431fd8817f2b67fb5d
BrokenInk <69753561+BrokenInks@users.noreply.github.com>
2021-08-09 10:25:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -24,22 +24,6 @@ client.categories = fs.readdirSync("./cmd/");
 
 const cooldowns = new Discord.Collection();
 
-client.on("message", async message => {
-    if (!message.guild) return;
-    if (message.author.bot) return;
-
-    const prefix = '?';
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    const randomXp = Math.floor(Math.random() * 9) + 1; //Random amont of XP until the number you want + 1
-    const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
-    if (hasLeveledUp) {
-        const user = await Levels.fetch(message.author.id, message.guild.id);
-        message.channel.send(`You leveled up to ${user.level}! Keep it going!`);
-    }
-    
 
 client.once('ready', () => {
 	console.log(`${client.user.tag} запустился!`);
