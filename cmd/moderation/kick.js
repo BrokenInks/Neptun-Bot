7d05e3a0de7026929ba33ac3f391840d19781047
BrokenInk <69753561+BrokenInks@users.noreply.github.com>
2021-10-07 19:04:04 +0300

    Add files via upload

--- cmd/moderation/kick.js
+++ cmd/moderation/kick.js
@@ -1,35 +1,33 @@
-const Discord = require("discord.js");
+const { Client } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const Discord = require('discord.js');
 const moment = require("moment");
 
 module.exports = {
-  name: "kick",
-  aliases: ["kick"],
-  category: "Bot",
-  description: "Kick",
-  usage: "kick",
-  cooldown: 15000,
-  run: async (bot, message, args, con) => {
-        message.delete();
+  name: 'kick',
+  cooldown: 5,
+  category: 'moderation',
+  execute(message, args, client) {
         var kickUser = message.guild.member(message.mentions.users.first() || message.guild.member(args[0]));
         if(!kickUser) return message.reply(`Вы не указали пользователя!`);
         var reason = args.slice(1).join(" ");
-        if(!reason) return message.reply(`Вы не указали причину!`);
+        if(!reason) return message.lineReply(`Вы не указали причину!`);
         if(!message.member.hasPermission(`KICK_MEMBERS`)) return message.reply(`недостаточно прав!`)
         if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply(`недостаточно прав у бота!`)//Проверка на права у бота на сервере
-        let reportchan = bot.channels.cache.get(`871475430304120832`);
+        let reportchan = client.channels.cache.get(`892772996677238784`);
         let embeddm = new Discord.MessageEmbed()
         .setTitle(`Вы успешно кикнули пользователя ${kickUser.user.tag}`)
-        .setFooter(`Desert Cave Development | Offical Bot`, bot.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+        .setFooter(`Offical bot by Broken`, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
         .setTimestamp();
         let embed = new Discord.MessageEmbed()
         .setTitle(`Пользователь ${kickUser.user.tag} был кикнут ${message.author.tag}`)
         .addField(`**Информация:**`, `**Кикнул: ${message.author.tag}[\`ID: ${message.author.id}\`]\nПользователя: ${kickUser.user.tag}[\`ID: ${kickUser.id}\`]**`)
         .addField(`**Причина:**`, `**\`${reason}\`**`)
-        .setFooter(`Desert Cave Development | Offical Bot`, bot.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+        .setFooter(`Desert Cave Development | Offical Bot`, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
         .setTimestamp();
-        if(kickUser.user.bot === true) return message.channel.send(`Нельзя кикнуть бота`);//Проверка на бота
-        await kickUser.send(embeddm);
-        await message.guild.member(kickUser).kick(reason);
-        await reportchan.send(embed);
+        if(kickUser.user.client === true) return message.channel.send(`Нельзя кикнуть бота`);//Проверка на бота
+        kickUser.send(embeddm);
+        message.guild.member(kickUser).kick(reason);
+        reportchan.send(embed);
     }
 }
